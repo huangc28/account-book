5934a5a02d218138cfa98167bf0282047e3461b1
Huang Chi-Han <huangchiheng@gmail.com>
2018-10-22 01:13:15 +0800

    Added dot env.

--- knexfile.js
+++ knexfile.js
@@ -1,47 +1,83 @@
+require('dotenv').config()
+
+
+const {
+  DB_HOST,
+  DB_PORT,
+  DB_NAME,
+  DB_USER,
+  DB_PASSWORD,
+} = process.env
+
+// console.log('process env', process.env)
+
+const dbConf = {
+  client: 'mysql',
+  connection: {
+    host: DB_HOST,
+    port: DB_PORT,
+    database: DB_NAME,
+    user: DB_USER,
+    password: DB_PASSWORD,
+  },
+  pool: {
+    max: 1,
+    min: 1,
+  },
+  migrations: {
+    tableName: 'knex_migrations',
+  },
+  seeds: {
+    directory: './seeds/dev',
+  },
+}
+
 // Update with your config settings.
-const DEVELOPMENT_CONFIG = {
-  client: 'mysql',
-  connection: {
-    host: '127.0.0.1',
-    port: '3306',
-    database: 'accounting_book',
-    user: 'root',
-    password: 'Huang_0216',
-  },
-  pool: {
-    max: 1,
-    min: 1,
-  },
-  migrations: {
-    tableName: 'knex_migrations',
-  },
-  seeds: {
-    directory: './seeds/dev',
-  },
-}
+// const DEVELOPMENT_CONFIG = {
+//   client: 'mysql',
+//   connection: {
+//     host: '127.0.0.1',
+//     port: '3306',
+//     database: 'accounting_book',
+//     user: 'root',
+//     password: 'Huang_0216',
+//   },
+//   pool: {
+//     max: 1,
+//     min: 1,
+//   },
+//   migrations: {
+//     tableName: 'knex_migrations',
+//   },
+//   seeds: {
+//     directory: './seeds/dev',
+//   },
+// }
 
-const TEST_CONFIG = {
-  client: 'mysql',
-  connection: {
-    host: '127.0.0.1',
-    port: '3306',
-    database: 'accounting_book',
-    user: 'root',
-    password: 'Huang_0216',
-  },
-  pool: {
-    max: 1,
-    min: 1,
-  },
-  migrations: {
-    tableName: 'knex_migrations',
-  },
-  seeds: {
-    directory: './seeds/dev',
-  },
-}
+// const TEST_CONFIG = {
+//   client: 'mysql',
+//   connection: {
+//     host: '127.0.0.1',
+//     port: '3306',
+//     database: 'accounting_book',
+//     user: 'root',
+//     password: 'Huang_0216',
+//   },
+//   pool: {
+//     max: 1,
+//     min: 1,
+//   },
+//   migrations: {
+//     tableName: 'knex_migrations',
+//   },
+//   seeds: {
+//     directory: './seeds/dev',
+//   },
+// }
 
-module.exports = {
-  development: DEVELOPMENT_CONFIG,
-  test: TEST_CONFIG,
-};
+// module.exports = {
+//   development: DEVELOPMENT_CONFIG,
+//   test: TEST_CONFIG,
+// };
+
+module.exports = dbConf
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import express from 'express'
+import expressGraphQL from 'express-graphql'
+import graphql from 'graphql'
 
 import v1Routers from './routes'
 
@@ -10,5 +12,54 @@ app.get('/health-check', (_, res) => {
 })
 
 app.use('/v1', v1Routers)
+
+const queryType = new graphql.GraphQLObjectType({
+  name: 'Query',
+  fields: {
+    rollDice: {
+      type: graphql.GraphQLList(graphql.GraphQLInt)
+    }
+  }
+})
+
+// GraphQL schema
+const schema = new graphql.GraphQLSchema({ query: queryType })
+
+// const schema = buildSchema(`
+//   type Query {
+//     rollDice(numDice: Int!, numSides: Int): [Int]
+//     quoteOfTheDay: String
+//     random: Float!
+//     rollThreeDice: [Int]
+//   }
+// `)
+
+// Root resolver
+// const rootResolver =  {
+//   rollDice: ({ numDice, numSides }) => {
+//     const output = [];
+
+//     for (let i = 0; i < numDice; i++) {
+//       output.push(1 + Math.floor(Math.random() * (numSides || 6)))
+//     }
+
+//     return output
+//   },
+//   quoteOfTheDay: () => {
+//     return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within'
+//   },
+//   random: () => {
+//     return Math.random()
+//   },
+//   rollThreeDice: () => {
+//     return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6))
+//   }
+// }
+
+app.use('/graphql', expressGraphQL({
+  schema,
+  rootValue: rootResolver,
+  graphiql: true,
+}))
 
 export default app
